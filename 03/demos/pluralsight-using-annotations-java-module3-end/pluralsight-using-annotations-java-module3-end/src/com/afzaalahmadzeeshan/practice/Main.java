cce20771f725ec71a5420cbd465f6a65924e95aa
CollinsTshuma <62089415+CollinsTshuma@users.noreply.github.com>
2021-06-27 12:20:25 +0200

    Initial commit

---
+++ 03/demos/pluralsight-using-annotations-java-module3-end/pluralsight-using-annotations-java-module3-end/src/com/afzaalahmadzeeshan/practice/Main.java
@@ -0,0 +1,10 @@
+package com.afzaalahmadzeeshan.practice;
+
+import com.afzaalahmadzeeshan.practice.models.Engineer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Code here.
+    }
+}
